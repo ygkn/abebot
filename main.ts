7647178a81a355b8ed2369fe0dfe1a3e4fdc65ce
ygkn <2000ygkn0713@gmail.com>
2021-11-15 18:31:02 +0900

    fix serving http

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import {
   join,
 } from "https://deno.land/std@0.78.0/path/mod.ts";
 import { startBot } from "https://deno.land/x/discordeno@12.0.1/mod.ts";
-import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import { Env } from "https://deno.land/x/env@v2.2.0/env.js";
 
 const env = new Env();
@@ -20,7 +20,9 @@ const readlines = async (filename: string): Promise<string[]> =>
     .filter((row) => row);
 
 const serveHttp = () =>
-  listenAndServe(`:${env.get("PORT", "8080")}`, () => new Response("online", { status: 200 }));
+  serve(() => new Response(`online`, { status: 200 }), {
+    addr: `${env.get("HOST", "0.0.0.0")}:${env.get("PORT", "8080")}`,
+  });
 
 const serveBot = async () => {
   const triggers = (await readlines("triggers.txt")).concat(
