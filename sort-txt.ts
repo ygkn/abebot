c39ea280a2d7a6664d9a7a6728bf5582204436c0
ygkn <2000ygkn0713@gmail.com>
2021-11-19 12:30:03 +0900

    add sort-txt script

---
+++ sort-txt.ts
@@ -0,0 +1,24 @@
+import {
+  dirname,
+  fromFileUrl,
+  join,
+} from "https://deno.land/std@0.78.0/path/mod.ts";
+
+const cwd = dirname(fromFileUrl(import.meta.url));
+
+Promise.all(["abe.txt", "triggers.txt"].map(async (filename) => {
+  const path = join(cwd, filename);
+
+  await Deno.writeTextFile(
+    path,
+    [
+      ...(await Deno.readTextFile(path))
+        .split("\n")
+        .map((row) => row.trim().toLowerCase())
+        .filter((row) => row)
+        .sort((a, b) => a.localeCompare(b)),
+      "",
+    ]
+      .join("\n"),
+  );
+}));
